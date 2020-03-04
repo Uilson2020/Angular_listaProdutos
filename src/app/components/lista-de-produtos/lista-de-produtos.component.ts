ef3ad203516c4ec9d071cc3da396095988b6c7d0
Treinamento <Treinamento@RD723932.raiadrogasil.com>
2020-03-04 17:34:57 -0300

    lista de produtos com angular

---
+++ src/app/components/lista-de-produtos/lista-de-produtos.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { Produto } from 'src/app/models/Produto';
+
+@Component({
+  selector: 'app-lista-de-produtos',
+  templateUrl: './lista-de-produtos.component.html',
+  styleUrls: ['./lista-de-produtos.component.css']
+})
+export class ListaDeProdutosComponent implements OnInit {
+  public produto: Produto [] = [];
+
+  constructor(private http: HttpService) { 
+    this.http.getProdutos().subscribe(
+      (data) => {this.produto = data;
+      }
+    );
+  }
+
+  ngOnInit(): void {
+  }
+
+}
